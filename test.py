8e4c6bf13febe410681ad412b496108f249cc1d6
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:13:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 def main():
   st.title('Streamlit web app')
-  counter = 0
+  if "counter" not in st.session_state:
+    st.session_state.counter = 0
   if st.button("Add 1"):
-    st.write(f"count {counter+1}")
+    st.session_state.counter +=1
+  st.write(f"count {st.session_state.counter+1}")
 
 if __name__ == "__main__":
   main()
