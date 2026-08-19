eb7bdacc0c6edbd4e54bb84648abb8df784cb764
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:35:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def main():
   
   number = st.number_input("Enter your number", value=1, min_value=1, max_value=12, key="number")
 
-  if st.button("convert"):
+  if st.button("multiply"):
    for i in range(1,13):
       st.write(f"{number} x {i} = {number * i}")
 
