cb8571de6eac354cf74a9a56f9edb9a4ff40cfe0
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:28:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,7 @@ def main():
   number = st.number_input("Enter your number", value=50, step=5, key='number')
   
   if st.button("divide by 5"):
-    
-    st.session_state.number = st.session_state.number/5
-  st.write(f"{st.session_state.number}")
+    st.write(f"{st.session_state.number/5}")
 
 if __name__ == "__main__":
   main()
