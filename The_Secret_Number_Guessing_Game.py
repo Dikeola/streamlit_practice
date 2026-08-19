8f6ae72d89c3d845c1d9acf38665dabe0d341dfb
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 16:20:28 +0100

    Update The_Secret_Number_Guessing_Game.py

--- The_Secret_Number_Guessing_Game.py
+++ The_Secret_Number_Guessing_Game.py
@@ -3,8 +3,20 @@ import random
 
 def main():
   st.title("The \"Secret Number\" Guessing Game")
-  if st.button("Random"):
+
+  number = random.randint(1,100)
+
+  guess = st.number_input("Guess The number")
+  if guess < number:
+    st.write("Too Low! Try again.")
     
+  elif guess > number:
+    st.write("Too High! Try again.")
+
+  else:
+    st.write("🎉 Correct! The number was X.")
+    
+  if st.button("Random"):
     st.write(random.randint(1,10))
 
 if __name__ == "__main__":
