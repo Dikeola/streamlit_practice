6351c0b066a1625c7943b9ca85d977a57207f20a
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:10:37 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
   st.title('Streamlit web app')
   counter = 0
-  if st.button(""):
+  if st.button("Add 1"):
     st.write(f"count {counter+1}")
 
 if __name__ == "__main__":
