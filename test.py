a3103fc1c18b43d174423d30217e64a57c6a6f74
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:18:57 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import streamlit as st 
 
-st.title("Streamlit WebApp")
+def main():
+  st.title("Streamlit WebApp")
+  
+  st.session_state.number = st.number_input(value=50, min_value=10,max_value=1000)
+  
+  if st.button("divide by 5"):
+    st.write(f"{st.session_state.number/5}")
 
-st.session_state.number = st.number_input(value=50, min_value=10,max_value=1000)
-
-if st.button("divide by 5"):
-  st.write(f"{st.session_state.number/5}")
+if __name__ : "__main__":
+  main()
