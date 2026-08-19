db010bf597afc018817c313e159b441f7afbb449
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:35:02 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import streamlit as st 
 
 def main():
-  st.title("Convert text to a list")
+  st.title("Multiplication Table")
   
-  text = st.text_input("Enter your text", key="text")
-  
-  
+  number = st.number_input("Enter your number", value=1, min_value=1, max_value=12, key="number")
+
   if st.button("convert"):
-    st.write(list(text))
+   for i in range(1,13):
+      st.write(f"{number} x {i} = {number * i}")
 
 if __name__ == "__main__":
   main()
