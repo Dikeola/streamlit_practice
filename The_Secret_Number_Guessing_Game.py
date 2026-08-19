ba0ab9a01b9b762755dd5ef4d4502eb724b6c0da
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 16:23:05 +0100

    Update The_Secret_Number_Guessing_Game.py

--- The_Secret_Number_Guessing_Game.py
+++ The_Secret_Number_Guessing_Game.py
@@ -15,9 +15,7 @@ def main():
 
   else:
     st.write("🎉 Correct! The number was X.")
-    
-  if st.button("Random"):
-    st.write(random.randint(1,10))
+    st.rerun()
 
 if __name__ == "__main__":
   main()
