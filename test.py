e138fc83c20ec418c9f61509101f17a881ecfd09
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:25:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import streamlit as st
 def main():
   st.title("Streamlit WebApp")
   
-  number = st.number_input(value=50)
-  st.session_state.number = number
+  number = st.number_input("Enter your number", value=50, step=5, key='number')
   
   if st.button("divide by 5"):
     st.write(f"{st.session_state.number/5}")
