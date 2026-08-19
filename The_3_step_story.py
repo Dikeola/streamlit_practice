f9bc6488bc74fde63460145bf0b2a5cd4c6b6ea7
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 09:49:15 +0100

    Update The_3_step_story.py

--- The_3_step_story.py
+++ The_3_step_story.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
   st.title("The 3-Step Story")
   if "screen" not in st.session_state:
-    screen = 1
+    st.session_state.screen = 1
 
   if st.session_state.screen == 1:
     st.write("You are standing in a dark forest. There is a path to the left and a cave to the right.")
@@ -19,6 +19,8 @@ def main():
       st.rerun
   if st.session_state.screen == 3:
     st.write("You walked right and fell into a trap! Game Over.")
+    if st.button("Play Again!"):
+      st.rerun
 
   
 if __name__ == "__main__":
