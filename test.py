d2ac8bf2097d60953bafbf361755daa441d05d2a
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:26:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def main():
   
   if st.button("divide by 5"):
     st.write(f"{st.session_state.number/5}")
-    st.session_state.number /=5
+    st.session_state.number = st.session_state.number/5
 
 if __name__ == "__main__":
   main()
