edd84fe6cf5298bd725f38e275b59d0950940988
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-19 03:17:18 +0100

    Fix syntax error in division output

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ st.title("Streamlit WebApp")
 st.session_state.number = st.number_input(value=50, min=10,max=1000)
 
 if st.button("divide by 5"):
-  st.write(f"{st.session_state.number/5}"
+  st.write(f"{st.session_state.number/5}")
