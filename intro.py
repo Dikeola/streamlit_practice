c1656129212b238e671e2cf50a0667553dd27e42
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-21 09:43:07 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,10 +1,9 @@
 import streamlit as st
+import random 
 
 def main():
   st.title("Introduction to Streamlit")
-
-  name = st.text_input("Enter your name:") 
-  st.write(f"Hello, {name}")
+  
   
 if __name__ == "__main__":
   main()
