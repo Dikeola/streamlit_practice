ce8747c039d61e6b6e7e784f952e610373d0c9b5
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-21 09:48:18 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -28,7 +28,7 @@ def main():
 ]
   if "random_word" not in st.session_state:
     st.session_state.random_word = random.choice(words)
-  st.session_state.guesses = list("_"*len(random_word))
+  st.session_state.guesses = list("_"*len(st.session_state.random_word))
 
   st.write(st.session_state.random_word)
   
