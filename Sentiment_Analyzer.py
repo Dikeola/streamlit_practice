477c89f532698d985c648a549c46e46859260204
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-26 13:47:04 +0100

    Update Sentiment_Analyzer.py

--- Sentiment_Analyzer.py
+++ Sentiment_Analyzer.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from sklearn.datasets import fetch_20newsgroups
+
+news = fetch_20newsgroups(subset='train', remove=('header','quotes','footer'), shuffle=True)
+news_df = pd.DataFrame({"text": news.data, 'labels':news.target})
+tfidf = TfidfVectorizer()
 
 def main():
   st.title("Sentiment Analyzer")
+  text = st.text_input("Enter your text")
+  tf_v = tfidf.fit_transform(text)
+  st.write(f"{pd.DataFrame(tf_v.toarray(), columns= tf_v.get_feature_names_out())}")
   
 if __name__ == "__main__":
   main()
