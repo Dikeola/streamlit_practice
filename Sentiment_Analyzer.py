ccf9946f6e3f2c7299d724530678ab32a0d1d2ba
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-26 13:51:14 +0100

    Implement button to generate sentiment table
    Added a button to generate sentiment table on demand.

--- Sentiment_Analyzer.py
+++ Sentiment_Analyzer.py
@@ -12,7 +12,9 @@ def main():
   st.title("Sentiment Analyzer")
   text = st.text_input("Enter your text")
   tf_v = tfidf.fit_transform(text)
-  st.write(f"{pd.DataFrame(tf_v.toarray(), columns= tf_v.get_feature_names_out())}")
+
+  if st.button("Generate Sentiment Table"):
+    st.write(f"{pd.DataFrame(tf_v.toarray(), columns= tf_v.get_feature_names_out())}")
   
 if __name__ == "__main__":
   main()
