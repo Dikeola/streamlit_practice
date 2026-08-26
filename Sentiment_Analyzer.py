338bb03917a2531e24d2bbe698988917a29b6f87
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-26 13:53:39 +0100

    Check for empty text input before TF-IDF transformation
    Added a check to ensure text input is not empty before fitting the TF-IDF model.

--- Sentiment_Analyzer.py
+++ Sentiment_Analyzer.py
@@ -12,8 +12,9 @@ tfidf = TfidfVectorizer()
 def main():
   st.title("Sentiment Analyzer")
   text = st.text_input("Enter your text")
-  tf_v = tfidf.fit_transform(pd.Series(text))
-
+  if !text.isEmpty():
+    tf_v = tfidf.fit_transform(pd.Series(text))
+  
   if st.button("Generate Sentiment Table"):
     st.write(f"{pd.DataFrame(tf_v.toarray(), columns= tf_v.get_feature_names_out())}")
   
