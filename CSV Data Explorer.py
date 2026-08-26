dc5ce8cbdbcd77ac7d20441cb76e52dd688196ca
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-26 14:16:27 +0100

    Create CSV Data Explorer.py

---
+++ CSV Data Explorer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import amtplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def main():
+  st.title("CSV Data Explorer")
+
+if __name__ == "__main__":
+  main()
