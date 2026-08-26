cd851b10a7a944949cc325d6a303475eda73a45a
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-26 14:39:32 +0100

    Add CSV file upload and display functionality

--- CSV Data Explorer.py
+++ CSV Data Explorer.py
@@ -6,5 +6,8 @@ import numpy as np
 def main():
   st.title("CSV Data Explorer")
   fle = st.file_uploader("Choose a CSV file:", type='csv')
+  if fle is not None:
+    df = pd.read_csv(fle)
+    st.DataFrame(df, use_container_width=True)
 if __name__ == "__main__":
   main()
