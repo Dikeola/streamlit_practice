6206e6dd67e622cac6ed2c5d732c728e0cecbcaa
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-08-28 11:50:06 +0100

    Update CSV Data Explorer_V2.py

--- CSV Data Explorer_V2.py
+++ CSV Data Explorer_V2.py
@@ -6,15 +6,12 @@ def main():
   st.title("CSV Data Explorer Version_2")
 
   fle = st.file_uploader("Upload your file:", type='csv')
-  lst = []
   
   if fle is not None:
     df = pd.read_csv(fle)
     
-    for col in df.columns:
-      lst.append(col)
     st.sidebar.header("Filter Options")
-    columns = st.sidebar.selectbox("Select the Columns:", lst)
+    columns = st.sidebar.selectbox("Select the Columns:", ['1','2'])
     st.dataframe(fle, use_container_width=True)
 if __name__ == "__main__":
   main()
